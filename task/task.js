e6dcc8d36634a0652b42edbb0e71eca4b58ce76e
muhammad nasuha <usulasal188@gmail.com>
2022-03-10 16:34:44 +0700

    pekan 2

--- task/task.js
+++ task/task.js
@@ -4,88 +4,138 @@
  * Object memiliki property: name, age, major.
  * Note: Ubah var menggunakan JavaScript Modern.
  */
-var users;
+
+ const users = [{
+  name: "Muhammad Nasuha M",
+  age: "20",
+  major: "Informatics",
+},
+{
+  name: "John Doe",
+  age: "22",
+  major: "Informatics",
+},
+{
+  name: "Steven",
+  age: "30",
+  major: "Informatics",
+},
+{
+  name: "Roger",
+  age: "23",
+  major: "Informatics",
+},
+{
+  name: "William",
+  age: "26",
+  major: "Informatics",
+},
+];
 
 /**
- * TODO 2
- * Buat function all: Menampilkan semua data user.
- * Hint: Gunakan for/for-of.
- * Note: Ubah function menggunakan arrow function.
- */
-function all() {}
+* TODO 2
+* Buat function all: Menampilkan semua data user.
+* Hint: Gunakan for/for-of.
+* Note: Ubah function menggunakan arrow function.
+*/
+
+const all = () => {
+for (let i = 0; i < users.length; i++) {
+  console.log(`Nama: ${users[i].name} 
+  Umur: ${users[i].age}
+  Major: ${users[i].major}`);
+}
+};
 
 /**
- * TODO 3
- * Buat function store: Menambahkan user baru.
- * Hint: Gunakan method push.
- * Note: Ubah function menggunakan arrow function.
- */
-function store(user) {}
+* TODO 3
+* Buat function store: Menambahkan user baru.
+* Hint: Gunakan method push.
+* Note: Ubah function menggunakan arrow function.
+*/
+const store = (user) => {
+users.push(user);
+all();
+};
 
 /**
- * TODO 4.
- * Buat function update: Mengedit data user.
- * Hint: re-assign array.
- * Note: Ubah function menggunakan arrow function.
- */
-function update(index, user) {}
+* TODO 4.
+* Buat function update: Mengedit data user.
+* Hint: re-assign array.
+* Note: Ubah function menggunakan arrow function.
+*/
+
+const update = (index, user) => {
+users[index] = user;
+all();
+};
 
 /**
- * TODO 5.
- * Buat function destroy: Menghapus data user.
- * Hint: Gunakan method splice.
- * Note: Ubah function menggunakan arrow function.
- */
-function destroy(index) {}
+* TODO 5.
+* Buat function destroy: Menghapus data user.
+* Hint: Gunakan method splice.
+* Note: Ubah function menggunakan arrow function.
+*/
+
+const destroy = (index) => {
+users.splice(index, 1);
+all();
+};
 
 /**
- * Function main.
- * Jangan edit atau hapus function main.
- * Function ini untuk testing task.
- */
+* Function main.
+* Jangan edit atau hapus function main.
+* Function ini untuk testing task.
+*/
 
 const main = () => {
-  /**
-   * Test function index
-   */
-  console.log("# Get All Users");
-  all();
+/**
+ * Test function index
+ */
+console.log("# Get All Users");
+all();
 
-  /**
-   * Test function store
-   */
-  console.log("# Add New User: Sabiq");
-  const newUser = {
-    name: "Sabiq",
-    age: 20,
-    major: "Informatics",
-  };
-  store(newUser);
+/**
+ * Test function store
+ */
+console.log("# Add New User: Sabiq");
+const newUser = {
+  name: "Sabiq",
+  age: 20,
+  major: "Informatics",
+};
+store(newUser);
 
-  /**
-   * Test function update
-   */
-  console.log("# Edit User: Isfa");
-  const editedIndex = 1;
-  const editedUser = {
-    name: "Isfhani Ghiyath",
-    age: 23,
-    major: "English",
-  };
-  update(editedIndex, editedUser);
+/**
+ * Test function update
+ */
+console.log("# Edit User: Isfa");
+const editedIndex = 1;
+const editedUser = {
+  name: "Isfhani Ghiyath",
+  age: 23,
+  major: "English",
+};
+update(editedIndex, editedUser);
 
-  /**
-   * Test function destroy
-   */
-  console.log("# Delete User: Nurul");
-  const deletedIndex = 2;
-  destroy(deletedIndex);
+/**
+ * Test function destroy
+ */
+console.log("# Delete User: Nurul");
+const deletedIndex = 2;
+destroy(deletedIndex);
 };
 
 main();
 
 /**
- * Jangan hapus exports.
- * Exports ini untuk tujuan testing.
- */
-module.exports = { users, all, store, update, destroy };
+* Jangan hapus exports.
+* Exports ini untuk tujuan testing.
+*/
+module.exports = {
+users,
+all,
+store,
+update,
+destroy
+};
